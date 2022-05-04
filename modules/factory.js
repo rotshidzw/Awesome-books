d1d142b515d3cd6e17408c89115b19ef0220b316
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-05-04 23:10:34 +0200

    adding linters  errors solution

--- modules/factory.js
+++ modules/factory.js
@@ -1,11 +1,11 @@
 export class Factory {
-    static retrieveBooks= () => {
-      const books = JSON.parse(localStorage.getItem('Books'));
-      document.getElementById('tbody').innerHTML = '';
-       for (let i = 0; i < books.length; i += 1) {
-        document.getElementById('tbody').innerHTML += `<tr><td class="book-container">"${books[i].title}" by ${books[i].author} <button class="btn deletebtn" type="button" id="id${i}">Remove</button></td></tr>`;
-      }
+  static retrieveBooks= () => {
+    const books = JSON.parse(localStorage.getItem('Books'));
+    document.getElementById('tbody').innerHTML = '';
+    for (let i = 0; i < books.length; i += 1) {
+      document.getElementById('tbody').innerHTML += `<tr><td class="book-container">"${books[i].title}" by ${books[i].author} <button class="btn deletebtn" type="button" id="id${i}">Remove</button></td></tr>`;
     }
+  }
 
       static createBook = (book) => {
         if (localStorage.getItem('Books') === null) {
